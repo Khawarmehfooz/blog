16d5f7c8bec8eef17b6f96c6d242a231a568a2c1
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-08-05 21:19:22 +0500

    Implement Edit Thumbnail Functionality

--- public/post/edit.php
+++ public/post/edit.php
@@ -15,7 +15,28 @@
         $post_title = $_POST['post-title'];
         $post_excerpt = $_POST['post-excerpt'] ?? '';
         $post_description = $_POST['post-description'] ?? '';
-        $post_thumbnail = "Will Do Later";
+        $post_thumbnail_path = "";
+        // File Uploading
+        $post_thumbnail = $_FILES['post-thumbnail'];
+        $post_thumbnail_name = $post_thumbnail['name'];
+        $post_thumbnail_temp_path = $post_thumbnail['tmp_name'];
+        $post_thumbnail_error = $post_thumbnail['error'];
+
+        if($post_thumbnail_error===UPLOAD_ERR_OK){
+            $thumbnail_upload_directory = getcwd() . "/uploads/";
+            $unique_thumbnail_name = uniqid(). "-" . $post_thumbnail_name;
+            $destination = $thumbnail_upload_directory . $unique_thumbnail_name;
+            $post_thumbnail_path = $unique_thumbnail_name;
+            // echo ;
+            $res = move_uploaded_file($post_thumbnail_temp_path,$destination);
+            if($res){
+                $_SESSION['message'] = "Thumbnail Uploaded";
+            }else{
+                echo "Error";
+            }
+        }else{
+            echo UPLOAD_ERR_NO_FILE;
+        }
 
         if(is_blank($post_title)){
             $errors[] = "Post Title cannot be blank!";
@@ -26,12 +47,12 @@
         if(is_blank($post_description)){
             $errors[] = "Post Description cannot be blank!";
         }
-        if(is_blank($post_thumbnail)){
+        if(is_blank($post_thumbnail_path)){
             $errors[] = "Choose a thumbnail!";
         }
 
         if(empty($errors)){
-            $result = update_post($id,$post_title,$post_excerpt,$post_description,$post_thumbnail);
+            $result = update_post($id,$post_title,$post_excerpt,$post_description,$post_thumbnail_path);
             if($result === true){
                 $_SESSION['message'] = "Post Updated Successfully!";
                 redirect_to(url_for("/user-dashboard"));
@@ -64,10 +85,13 @@
     <div class="message-container">
         <?php echo display_errors($errors); ?>
     </div>
-    <form action="<?php echo url_for("/post/edit.php?id={$post_id}") ?>" class="create-post-form" method="POST">
+    <form action="<?php echo url_for("/post/edit.php?id={$post_id}") ?>" class="create-post-form" method="POST" enctype="multipart/form-data">
         <input type="text" name="post-title" id="post-title" placeholder="Title" value="<?php echo $post['post_title']; ?>">
         <input type="text" name="post-excerpt" id="post-excerpt" placeholder="Except" value="<?php echo $post['post_excerpt']; ?>">
-        <!-- <input type="file" name="post-image" id="post-image"> -->
+        <h2>Current Thumbnail</h2><br>
+        <img src="<?php echo url_for("/post/uploads/{$post['post_thumbnail']}")?>" id="current-thumbnail" alt=""><br>
+        <label for="post-thumbnail"><h2>Choose New Thumbnail</h2></label>
+        <input type="file" name="post-thumbnail" id="post-thumbnail" accept="image/*" value="<?php echo url_for("/post/uploads/{$post['post_thumbnail']}") ?>">
         <textarea name="post-description" id="post-description" cols="30" rows="10" >
             <?php echo $post['post_description'];?>
         </textarea>
